0146717cbc1dfcfe0862671da9651223245c0f6f
Viktor Wase <viktorwase@gmail.com>
2019-02-08 21:23:28 +0100

    Fix mutate function s.t. second arg of unary operation doesn't count as active

--- optimization.py
+++ optimization.py
@@ -97,7 +97,14 @@ def mutate(cgp_in, dims, nr_of_funcs):
 		new_gene[i] = randint(0, max_vals[i])
 
 		if is_active and new_gene[i]!=cgp_in.gene[i]:
-			has_mutated_used_node=True
+			# If the second input is changed then we need to check 
+			# that it is a binary operation
+			if i%3==2:
+				op = cgp.op_table[new_gene[i-2]]
+				if op.is_binary:
+					has_mutated_used_node = True
+			else:
+				has_mutated_used_node=True
 
 	# Create the new CGP object
 	new_cgp = CGP(cgp_in.dims, cgp_in.op_table, new_gene, nr_of_parameters=cgp_in.nr_of_parameters, fast_setup = not cgp_in.has_setup_used_nodes)
