a691404a2e3a3bdc0db513a49716be06bb084468
Viktor Wase <viktorwase@gmail.com>
2019-07-28 22:29:30 +0200

    New Neural network, bugfix in binary search and some other stuff.

--- ann.py
+++ ann.py
@@ -1,13 +1,16 @@
-from random import gauss, shuffle, seed
-from math import fabs
+from random import shuffle, seed
+from math import fabs, pi
 
 from os import listdir
+
+import scipy.io as sio
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 import torch.optim as optim
+
 
 class Net(nn.Module):
 	def __init__(self, channels=10):
@@ -21,20 +24,21 @@ class Net(nn.Module):
 		self.outs = 1
 
 		self.fc1 = nn.Linear(self.inps, self.lay1)
-		#self.d1 = nn.Dropout(p=0.04)
+		self.d1 = nn.Dropout(p=0.0)
 		self.fc2 = nn.Linear(self.lay1, self.lay2)
-		#self.d2 = nn.Dropout(p=0.04)
-		self.fc3 = nn.Linear(self.lay2, self.lay3)
+		self.d2 = nn.Dropout(p=0.0)
+		#self.fc3 = nn.Linear(self.lay2, self.lay3)
 		#self.d3 = nn.Dropout(p=0.04)
 		self.fc4 = nn.Linear(self.lay3, self.outs)
 
 	def forward(self, x):
-		x = F.relu(self.fc1(x))
-		x = F.relu(self.fc2(x))
-		x = F.relu(self.fc3(x))
+		x = self.d1(F.relu(self.fc1(x)))
+		x = self.d2(F.relu(self.fc2(x)))
+		#x = F.relu(self.fc3(x))
 		x = self.fc4(x)
 
 		return x
+
 
 def get_itd(left, right, sample_rate=1.0/44100.0, p=0.9):
 	left_mag = [fabs(el) for el in left]
@@ -47,26 +51,25 @@ def get_itd(left, right, sample_rate=1.0/44100.0, p=0.9):
 
 	assert len(left) == len(right)
 	for i in range(len(left)):
-		if left_mag[i] >= 0.9*l_max and left_onset==None:
+		if left_mag[i] >= 0.9*l_max and left_onset is None:
 			left_onset = i*sample_rate
-		if right_mag[i] >= 0.9*r_max and right_onset==None:
+		if right_mag[i] >= 0.9*r_max and right_onset is None:
 			right_onset = i*sample_rate
 
-		if left_onset!=None and right_onset!=None:
+		if left_onset is not None and right_onset is not None:
 			break
 
 	return left_onset-right_onset
 
-def get_head_size(id):
-	return 1.0 # TODO: Write.
 
-def get_data(ids):
+def get_data(ids, head_sizes):
 	print("Reading data")
 	itds = []
 	inps = []
+	i = 0
 	for id in ids:
 		print(id)
-		head_size = get_head_size(id)
+		head_size = head_sizes[i]
 		files = listdir("itd_data/"+id)
 		for file in files:
 			with open("itd_data/"+id+"/"+file) as f:
@@ -83,13 +86,14 @@ def get_data(ids):
 
 				while True:
 					line = f.readline().split("\t")
-					if len(line)==2:
+					if len(line) == 2:
 						left.append(float(line[0]))
 						right.append(float(line[1]))
 					else:
 						break
-			itds.append([get_itd(left, right)])
-			inps.append([azi, ele, head_size])
+			itds.append([44100*get_itd(left, right)])
+			inps.append([azi*pi/180.0, ele*pi/180.0, head_size])
+		i += 1
 
 	assert len(itds) == len(inps)
 	n = len(inps)
@@ -103,77 +107,130 @@ def get_data(ids):
 
 
 if __name__ == '__main__':
+	torch.set_default_dtype(torch.float64)
+	anthro_mat = sio.loadmat('itd_data/anthropometry/anthro.mat')
+
+	# Get the IDs. ach should have 3 numbers.
+	head_sizes = [x[0] for x in anthro_mat['X']]
+	should_keep = [size > 0 for size in head_sizes]
+
 	seed(0)
 	IDs = ['003', '008', '009', '010', '011', '012', '015', '017', '018', '019', '020', '021', '027', '028', '033', '040', '044', '048', '050', '051', '058', '059', '060', '061', '065', '119', '124', '126', '127', '131', '133', '134', '135', '137', '147', '148', '152', '153', '154', '155', '156', '158', '162', '163', '165']
 
+	IDs = list(filter(lambda x: should_keep[IDs.index(x)], IDs))
+	head_sizes = list(filter(lambda x: x > 0, head_sizes))
+
 	idxs = [i for i in range(len(IDs))]
 	shuffle(idxs)
-	assert len(IDs) == 45
-	train_IDS = [IDs[idxs[i]] for i in range(30)]
-	test_IDS = [IDs[idxs[i]] for i in range(30, len(IDs))]
+	print(len(IDs))
+	assert len(IDs) == 45 - 8
+	assert len(head_sizes) == len(IDs)
+	train_IDS = [IDs[idxs[i]] for i in range(27)]
+	test_IDS = [IDs[idxs[i]] for i in range(27, len(IDs))]
 
-	inps, itds = get_data(train_IDS)
+	train_head_sizes = [head_sizes[idxs[i]] for i in range(27)]
+	test_head_sizes = [head_sizes[idxs[i]] for i in range(27, len(IDs))]
 
-	from math import sin
+	inps, itds = get_data(train_IDS, train_head_sizes)
 
 	errors = []
-	for channels in range(1, 10):
+	# for channels in range(5, 8):
+	for channels in range(10, 11):
 		net = Net(channels)
-
-		#print(sum([fabs(el[0]) for el in itds])/len(itds))
+		net.train(True)
 
 		crit = nn.MSELoss()
-		optimizer = optim.Adam(net.parameters())
+		optimizer = optim.Adam(net.parameters(), lr=1.0e-4, weight_decay=0.1)
 		print("Starting training")
 
 		c = 0
-		batchsize = 32
-		inps_tensor = []
-		outs_true = []
-		for epoch in range(100):
-			loss_sum = 0.0
-			for i in range(len(inps)):
-				# TODO: Batchsize
+		batchsize = 16
+		inps_tensor = [None]*batchsize
+		outs_true = [None]*batchsize
+		for epoch in range(1000):
+			loss_epoch = 0.0
 
-				inps_tensor.append(inps[i])
-				outs_true.append(itds[i])
+			assert len(inps) == len(itds)
+			idx = [i for i in range(len(inps))]
+			shuffle(idx)
+			for i in range(len(inps)):
+				inps_tensor[c] = inps[idx[i]]
+				outs_true[c] = itds[idx[i]]
+
+				# inps_tensor.append(inps[i])
+				# outs_true.append(itds[i])
 
 				c += 1
-				if c==batchsize:
+				if c == batchsize:
 					optimizer.zero_grad()
-					
-					inps_tensor = torch.FloatTensor(inps_tensor)
-					outs_true = torch.FloatTensor(outs_true)
+
+					inps_tensor = torch.DoubleTensor(inps_tensor)
+					outs_true = torch.DoubleTensor(outs_true)
 					outs = net(inps_tensor)
 
 					loss = crit(outs, outs_true)
-					loss_sum += loss.item()
-
-
 					loss.backward()
+					loss_epoch += loss.item()
 					optimizer.step()
 
-
-					inps_tensor = []
-					outs_true = []
+					inps_tensor = [None]*batchsize
+					outs_true = [None]*batchsize
 					c = 0
-
-
-			#if epoch%1==0:
-			#	print(loss_sum)
+			print(channels, epoch, loss_epoch)
 
 		# Validation
-		print("Starting Validation")
+		print("Starting Validation (on train set)")
 		net.train(False)
 		error = 0.0
 		for i in range(len(inps)):
-			inps_tensor = torch.FloatTensor(inps[i])
-			outs_true = torch.FloatTensor(itds[i])
+			inps_tensor = torch.DoubleTensor(inps[i])
+			outs_true = torch.DoubleTensor(itds[i])
 			outs = net(inps_tensor)
 
-			error += fabs(outs.item()-outs_true)/ len(inps)
-		errors.append(error)
-		print("Error of", channels, "channels:", error)
+			error += fabs(outs.item()-outs_true)
+		errors.append(error/len(inps))
+		print("Error of", channels, "channels:", error/len(inps))
+
+		print("Starting Validation (on test set)")
+		net.train(False)
+		error = 0.0
+		inps, itds = get_data(test_IDS, test_head_sizes)
+
+		for i in range(len(inps)):
+			inps_tensor = torch.DoubleTensor(inps[i])
+			outs_true = torch.DoubleTensor(itds[i])
+			outs = net(inps_tensor)
+
+			#print(outs[0], outs_true[0])
+
+			error += fabs(outs.item()-outs_true)
+		# errors.append(error/len(inps))
+		print("Error of", channels, "channels:", error/len(inps))
+
+		#torch.save(net, "ANN")
 	print(errors)
 
+	# import matplotlib.pyplot as plt
+	# plt.plot(errors)
+	# plt.show()
 
+	import pickle
+	pickle.dump(net, open('net_weight01_nr2.p', 'wb'))
+
+	torch.save(net, 'net_weight01_nr2')
+
+	"""Error of 10 channels: 2.0205890134210533
+	Starting Validation (on test set)
+	Reading data
+	163
+	135
+	152
+	051
+	154
+	155
+	061
+	018
+	137
+	134
+	Error of 10 channels: 2.3101025420778467
+	[2.0205890134210533]"""
--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 from random import gauss
 from copy import copy
-from time import time
-from math import sqrt, fabs, exp
-inf = float('inf')
+from time import time, perf_counter
+from math import sqrt, fabs
 from newton_raphson import newton_raphson
 from cgpy.cgp import CGP
 from optimization import multistart_opt
 from pso import pso
 from taylor_approx import TaylorApprox
+inf = float('inf')
 
 
 def calc_alpha_for_root(func, par_vals, x_root, h=1.0e-10):
@@ -21,18 +21,20 @@ def calc_alpha_for_root(func, par_vals, x_root, h=1.0e-10):
 
 	return min(fabs(2.0*f_second_der/(f_der+1.0e-10)), 1.0/(x_root*x_root+1.0e-10))
 
+
 def projected_new_error(nr_of_newton_iters, start_err, alpha):
 	# So yeah, this is hard to explain. TODO: Write a good explanation.
 	exponent = pow(2.0, nr_of_newton_iters)
-	if alpha==0:
+	if alpha == 0:
 		return 0
 	return pow(alpha, exponent-1)*pow(start_err, exponent)
 
+
 def calc_computational_baseline_operations(n, f, is_binary):
-	numbers = [fabs(gauss(0,1))+1.0e8 for _ in range(n)]
+	numbers = [fabs(gauss(0, 1))+1.0e8 for _ in range(n)]
 
 	if is_binary:
-		second_numbers = [fabs(gauss(0,1))+1.0e8 for _ in range(n)]
+		second_numbers = [fabs(gauss(0, 1))+1.0e8 for _ in range(n)]
 
 	t0 = time()
 	if is_binary:
@@ -45,6 +47,7 @@ def calc_computational_baseline_operations(n, f, is_binary):
 
 	return t/float(n)
 
+
 def calc_the_number_of_newton_iters_that_are_lost(cgp, running_times_ops, running_time_NR):
 	total_time = 0.0
 	nr_of_nodes = int((len(cgp.gene)-1)/3)
@@ -56,6 +59,7 @@ def calc_the_number_of_newton_iters_that_are_lost(cgp, running_times_ops, runnin
 			total_time += running_times_ops[op_nr]
 	newton_iters = total_time / running_time_NR
 	return newton_iters
+
 
 def objective_func_cont(x, data):
 	"""
@@ -82,15 +86,14 @@ def objective_func_cont(x, data):
 	nr_of_newton_iters = calc_the_number_of_newton_iters_that_are_lost(cgp, running_times_ops, running_time_NR)
 
 	# One could imagine a function f, with one variable x, and two parameters a & b,
-	# where the function is f(x) = b*x. Which means that the parameter a is unused. This means 
-	# that it doesn't have to be tuned, but the function still needs two parameters (at least it 
+	# where the function is f(x) = b*x. Which means that the parameter a is unused. This means
+	# that it doesn't have to be tuned, but the function still needs two parameters (at least it
 	# thinks that it does). In these cases we just set a:=0 and tune only b.
 
 	def func(X, parameters=[]):
 		# TODO: This shouldn't have to be done every time.
 		# Insert zeroes in the non-used parameters
 		pars = [0.0 for _ in range(cgp.nr_of_parameters)]
-		#print(x, parameters, nr_of_pars_used, cgp.nr_of_parameters)
 		assert len(parameters) == nr_of_pars_used
 
 		counter = 0
@@ -101,7 +104,7 @@ def objective_func_cont(x, data):
 
 		try:
 			return cgp.eval(X, parameters=pars)
-		except (ValueError, ZeroDivisionError): # Sometimes x gets really big and this can cause problems.
+		except (ValueError, ZeroDivisionError):  # Sometimes x gets really big and this can cause problems.
 			print("Math domain error in start error func.")
 			return 1.0e20
 
@@ -109,12 +112,11 @@ def objective_func_cont(x, data):
 	assert len(par_samples) == n
 
 	total_error = 0.0
-	total_time_error = 0.0
 	for i in range(n):
 		root = roots[i]
 		par_sample = par_samples[i]
 
-		root_guess = func(par_sample, x) # Yes, I know that par_sample goes to cgp.x, and x goes to cgp.parameters. The naming is terrible, but it is the way it should be.
+		root_guess = func(par_sample, x)  # Yes, I know that par_sample goes to cgp.x, and x goes to cgp.parameters. The naming is terrible, but it is the way it should be.
 		err = fabs(root_guess-root)
 
 		proj_err = projected_new_error(nr_of_newton_iters, fabs(root_guess-root), conv_factors[i])
@@ -127,29 +129,28 @@ def objective_func_cont(x, data):
 
 	return sqrt(total_error/n)
 
+
 def objective_func_disc(f_vals, pnts, dims, new_cgp, nr_of_pars, op_table, data):
-	cgp = copy(new_cgp) # TODO: Is this copy really needed?
+	assert len(data) == 5
+	cgp = copy(new_cgp)  # TODO: Is this copy really needed?
 	# Then we find out which parameters that are used.
 	is_par_used = cgp.which_parameters_are_used()
 	nr_of_pars_used = sum(is_par_used)
 	data = [new_cgp] + data
-	
+
 	# We ignore the constant CGPs, because they are dumb and treated separately.
 	if cgp.is_constant:
 		return (inf, [None]*nr_of_pars)
 
 	if nr_of_pars_used > 0:
 		# If some parameters are used in the model, then these will have to be tuned.
-		
 		# Then we do a curve-fitting of the numerical constants/parameters.
-		nr_of_pnts = len(pnts)
-
 		err_func = lambda x: objective_func_cont(x, data)
 		inds = 20
 		max_iter = 50
 		(error, best_par_vals) = pso(nr_of_pars_used, inds, max_iter, err_func)
 
-		#(best_par_vals, error) = combo_curve_fitting(residual_func, f_vals, nr_of_pars_used, nr_of_pnts, pnts, jacobian_func, func)
+		# (best_par_vals, error) = combo_curve_fitting(residual_func, f_vals, nr_of_pars_used, nr_of_pnts, pnts, jacobian_func, func)
 
 		# If some parameters are unused, then we'll just set them to 0.
 		if len(best_par_vals) != nr_of_pars:
@@ -164,7 +165,6 @@ def objective_func_disc(f_vals, pnts, dims, new_cgp, nr_of_pars, op_table, data)
 			best_par_vals = best_par_vals_padded
 	else:
 		# If no parameter is actually used, then there is no need for any curve-fitting.
-		func = cgp.eval
 
 		# Okay, so no parameters are actually used, so we'll just use some dummy values.
 		pars = [0.0 for _ in range(cgp.nr_of_parameters)]
@@ -172,6 +172,7 @@ def objective_func_disc(f_vals, pnts, dims, new_cgp, nr_of_pars, op_table, data)
 		best_par_vals = pars
 
 	return (error, best_par_vals)
+
 
 def how_many_genes_exists(dims, nr_of_nodes, ignore_id=True):
 	from operation_table import op_table
@@ -195,15 +196,17 @@ def how_many_genes_exists(dims, nr_of_nodes, ignore_id=True):
 
 	return counter
 
-def generate_parameters(nr_of_parameters, parameter_generator, n=500):
-	assert n>0
+
+def generate_parameters(nr_of_parameters, parameter_generator, n=50):
+	assert n > 0
 	assert nr_of_parameters > 0
 	parameter_samples_full = [parameter_generator() for _ in range(n)]
 	for tmp in parameter_samples_full:
 		assert len(tmp) == nr_of_parameters
 	return parameter_samples_full
 
-def binary_search_for_root(func, trials=1000, binary_search_iters=1000, sd=100, thresh=1.0e-12):
+
+def binary_search_for_root(func, trials=1000, binary_search_iters=1000, sd=100, thresh=1.0e-14):
 	has_found_pos = False
 	has_found_neg = False
 	# Randomly search for a negative and positive pnt.
@@ -211,26 +214,25 @@ def binary_search_for_root(func, trials=1000, binary_search_iters=1000, sd=100, 
 		if has_found_neg and has_found_pos:
 			break
 		x = gauss(0, 100)
-		try:
-			val = func(x)
-		except:
-			val = None
-		if val!=None:
-			if not has_found_pos and val>0:
+		val = func([x])
+		if val is not None:
+			if not has_found_pos and val > 0:
 				pos_x = x
-			elif not has_found_neg and val<0:
+				has_found_pos = True
+			elif not has_found_neg and val < 0:
+				has_found_neg = True
 				neg_x = x
-	if has_found_neg==False or has_found_pos==False:
+	if has_found_neg is False or has_found_pos is False:
 		return False
 
 	# Do binary search to find a root.
 	# This assumes that the function is continous.
 	for i in range(binary_search_iters):
 		x_mid = (pos_x+neg_x)*0.5
-		val = func(x_mid)
-		if fabs(val)<=thresh:
+		val = func([x_mid])
+		if fabs(val) <= thresh:
 			return x_mid, val
-		if val>0:
+		if val > 0:
 			pos_x = x_mid
 		else:
 			neg_x = x_mid
@@ -248,23 +250,26 @@ def calc_roots(func, func_der, parameter_samples_full):
 	for res in newt_rap_results:
 		root, error = res
 		# Remove all roots that didn't converge.
-		if error<1.0e-12:
+		if error < 1.0e-12:
 			roots.append(root)
 			parameter_samples.append(parameter_samples_full[i])
 		else:
-			binary_search_res = binary_search_for_root(func)
-			if binary_search_res==False:
+			parameter = parameter_samples_full[i]
+			func_curry = lambda x: func(x, parameter)
+			binary_search_res = binary_search_for_root(func_curry)  # Don't forget to rerun this with exp 1-3 since there was a bug.
+			if binary_search_res is False:
 				nr_of_non_converged += 1
 				pass
 			else:
-				print("YEPP---------------")
 				roots.append(binary_search_res[0])
 				parameter_samples.append(binary_search_res[0])
 		i += 1
 	return roots, parameter_samples, nr_of_non_converged
 
+
 def calc_baseline(roots, parameter_samples, nr_of_comp_iters=10000):
 	# Get computational baseline for NR
+	assert len(roots) > 0
 
 	assert len(roots) == len(parameter_samples)
 	total_time = 0.0
@@ -306,8 +311,8 @@ def calc_baseline(roots, parameter_samples, nr_of_comp_iters=10000):
 
 	# Get computational baseline for all operations
 	# TODO: Create these automatically
-	ops = [lambda x: sin(x), lambda x: cos(x), lambda x,y: x+y, lambda x,y: x*y, lambda x: x*x, lambda x,y: x-y,  lambda x,y: x/y, lambda x: x]
-	is_binary_list = [False, False, True,True,False,True,True,False]
+	ops = [lambda x: sin(x), lambda x: cos(x), lambda x, y: x+y, lambda x, y: x*y, lambda x: x*x, lambda x, y: x-y, lambda x, y: x/y, lambda x: x]
+	is_binary_list = [False, False, True, True, False, True, True, False]
 	assert len(is_binary_list) == len(ops)
 
 	nr_of_comp_iters_ops = nr_of_comp_iters*100
@@ -319,16 +324,19 @@ def calc_baseline(roots, parameter_samples, nr_of_comp_iters=10000):
 	print("The other running times are:", ops_running_times)
 	return ops_running_times, total_time
 
+
 def calc_baselines_and_roots(func, func_der, parameter_generator, nr_of_parameters):
 	# Get parameter samples
 	parameter_samples_full = generate_parameters(nr_of_parameters, parameter_generator)
-
+	print("1")
 	# Find the roots for each parameter sample
 	roots, parameter_samples, nr_of_non_converged = calc_roots(func, func_der, parameter_samples_full)
-
+	assert len(roots) > 0
+	print("2")
 	# Get computational baseline for NR
 	ops_running_times, total_time = calc_baseline(roots, parameter_samples)
-	
+	print("3")
+
 	# Calculate the convergance factor of NR for each
 	# of the found roots.
 	# TODO: The following only works if the derivative is non-zero at the root. Generalize somehow!
@@ -346,10 +354,12 @@ def calc_baselines_and_roots(func, func_der, parameter_generator, nr_of_paramete
 
 	return optimization_data
 
+
 def get_constant_solution(roots):
 	n = len(roots)
 
 	return sum(roots)/float(n)
+
 
 def cgp_find_starting_guess(optimization_data, func, func_der,	parameter_generator, nr_of_parameters, max_time_sec=8*60*60):
 	print("Starting cgp optimization algo.")
@@ -364,17 +374,19 @@ def cgp_find_starting_guess(optimization_data, func, func_der,	parameter_generat
 
 	return (best_sol, best_err, best_pars)
 
+
 if __name__ == '__main__':
 	from random import random
 	from math import pi, sin, cos, acos, sqrt, asin
 	from scipy.special import erf
 	X_NEEDS_TO_BE_POS = False
+	print("Starting")
 
 	# EXP 1 - Planetary ellipse position
-	#func = lambda x, beta: x[0] - beta[0]*sin(x[0]) - beta[1]
-	#func_der = lambda x, beta: 1.0 - beta[0]*cos(x[0])
-	#parameter_generator = lambda : [random()*0.1, random()*2*pi]
-	#nr_of_parameters = 2
+	# func = lambda x, beta: x[0] - beta[0]*sin(x[0]) - beta[1]
+	# func_der = lambda x, beta: 1.0 - beta[0]*cos(x[0])
+	# parameter_generator = lambda : [random()*0.1, random()*2*pi]
+	# nr_of_parameters = 2
 
 	# EXP 2 - sqrt in range [0, 1]
 	func = lambda x, beta: x[0]*x[0] - beta[0]
@@ -383,31 +395,94 @@ if __name__ == '__main__':
 	nr_of_parameters = 1
 
 	# EXP 3 - MGA Flyby calculation
-	#X_NEEDS_TO_BE_POS = True
-	#func = lambda x, beta: asin(beta[0]/(beta[0]+x[0]))+asin(beta[1]/(beta[1]+x[0]))-beta[2]
-	#func_der = lambda x, beta: -beta[0]/((beta[0]+x[0])*sqrt(x[0]*(2.0*beta[0]+x[0]))) - beta[1]/((beta[1]+x[0])*sqrt(x[0]*(2.0*beta[1]+x[0])))
-	#parameter_generator = lambda : [1.0/((random()*1.5+0.5)**2), 1.0/((random()*1.5+0.5)**2), random()*pi*0.5]
-	#nr_of_parameters = 3
+	# X_NEEDS_TO_BE_POS = True
+	# func = lambda x, beta: asin(beta[0]/(beta[0]+x[0]))+asin(beta[1]/(beta[1]+x[0]))-beta[2]
+	# func_der = lambda x, beta: -beta[0]/((beta[0]+x[0])*sqrt(x[0]*(2.0*beta[0]+x[0]))) - beta[1]/((beta[1]+x[0])*sqrt(x[0]*(2.0*beta[1]+x[0])))
+	# parameter_generator = lambda : [1.0/((random()*1.5+0.5)**2), 1.0/((random()*1.5+0.5)**2), random()*pi*0.5]
+	# nr_of_parameters = 3
 
 	# EXP 4 - Normal propabillity calculation
-	#func = lambda x, beta: -0.5*erf((beta[0]-x[0])/(1.41421356237*beta[1])) - beta[2]
-	#func_der = lambda x, beta: exp(-(x[0] - beta[0])*(x[0] - beta[0])/(2.0*beta[1]*beta[1]))/(2.50662827463*beta[1])
-	#parameter_generator = lambda : [gauss(0,1), fabs(gauss(0,1)), random()*0.9999+(1-0.9999)/2.0]
-	#nr_of_parameters = 3
+	# func = lambda x, beta: -0.5*erf((beta[0]-x[0])/(1.41421356237*beta[1])) - beta[2]
+	# func_der = lambda x, beta: exp(-(x[0] - beta[0])*(x[0] - beta[0])/(2.0*beta[1]*beta[1]))/(2.50662827463*beta[1])
+	# parameter_generator = lambda : [gauss(0,1), fabs(gauss(0,1)), random()*0.9999+(1-0.9999)/2.0]
+	# nr_of_parameters = 3
+
+	"""
+	# EXP 5 - ANN for ITD
+	import torch
+	from ann import Net
+	from scipy.interpolate import LinearNDInterpolator
+
+	# net = torch.load("ANN")
+	net = torch.load("net_weight01_nr2")
+	# x is azi, beta is [ele,  head_size, itd]
+
+	def func(x, beta):
+		tmp = net(torch.DoubleTensor(x+beta[0:2])).item()
+		return tmp - beta[2]
+
+	def func_der(x, beta):
+		out = (net(torch.DoubleTensor([x[0]+1.0e-6]+beta[0:2])) - net(torch.DoubleTensor([x[0]-1.0e-6]+beta[0:2]))).item() / 2.0e-6
+		return out.item()
+
+	def parameter_generator_full(cipic_data, cipic_data_itd):
+		itd = float('nan')
+		interpolator = LinearNDInterpolator(cipic_data, cipic_data_itd)
+
+		while isnan(itd):  # TODO: Make sure that the cyclic variables are taken care of.
+			azi = random()*pi  # TODO: I don't think this is correct
+			ele = random()*(180+45)/180*pi-pi/4
+			head_size = gauss(14.49, 0.95)
+			X = [azi, ele, head_size]
+
+			itd_interpol = interpolator(X)
+			itd = itd_interpol[0][0]
+			print("itd", itd)
+
+		return [ele, head_size, itd]
+
+	import scipy.io as sio
+	from ann import get_data
+	import numpy as np
+	from math import isnan
+	IDs = ['003', '008', '009', '010', '011', '012', '015', '017', '018', '019', '020', '021', '027', '028', '033', '040', '044', '048', '050', '051', '058', '059', '060', '061', '065', '119', '124', '126', '127', '131', '133', '134', '135', '137', '147', '148', '152', '153', '154', '155', '156', '158', '162', '163', '165']
+	anthro_mat = sio.loadmat('itd_data/anthropometry/anthro.mat')
+
+	# Get the IDs. ach should have 3 numbers.
+	head_sizes = [x[0] for x in anthro_mat['X']]
+
+	# Remove all subjects with missing data
+	counter = 0
+	while counter < len(head_sizes):
+		if isnan(head_sizes[counter]):
+			head_sizes.pop(counter)
+			IDs.pop(counter)
+		else:
+			counter += 1
+	assert len(IDs) == len(head_sizes)
+	inps, itds = get_data(IDs, head_sizes)
+
+	parameter_generator = lambda: parameter_generator_full(inps, itds)
+	nr_of_parameters = 3
+	"""
+
+	# func = lambda x, P: acos(1.0/ ( x[0]/P[0] - 1.0))+acos(1.0/ ( x[0]/P[1] - 1.0))-P[2] if x[0] >= 0 else 1.0e10
+	# func_der = lambda x, P: P[0] /( (x[0]-P[0])*(x[0]-P[0])*sqrt(1.0 - (P[0]/(x[0]-P[0]))**2))+P[1]/( (x[0]-P[1])*(x[0]-P[1])*sqrt(1.0 - (P[1]/(x[0]-P[1]))**2)) if x[0] >= 0 else 1.0
+	# parameter_generator = lambda :  [-0.9*random()-0.05, -0.9*random()-0.05 ,pi+pi*random()]
+	# nr_of_parameters = 3
 
 
-	#func = lambda x, P: acos(1.0/ ( x[0]/P[0] - 1.0))+acos(1.0/ ( x[0]/P[1] - 1.0))-P[2] if x[0] >= 0 else 1.0e10
-	#func_der = lambda x, P: P[0] /( (x[0]-P[0])*(x[0]-P[0])*sqrt(1.0 - (P[0]/(x[0]-P[0]))**2))+P[1]/( (x[0]-P[1])*(x[0]-P[1])*sqrt(1.0 - (P[1]/(x[0]-P[1]))**2)) if x[0] >= 0 else 1.0
-	#parameter_generator = lambda :  [-0.9*random()-0.05, -0.9*random()-0.05 ,pi+pi*random()]
-	#nr_of_parameters = 3
+	print("Calc baselines and roots")
 	opt_data = calc_baselines_and_roots(func, func_der, parameter_generator, nr_of_parameters)
 	[conv_factors, total_time, ops_running_times, parameter_samples, roots] = opt_data
 
+	print("Calc Taylor Approx")
 	tay = TaylorApprox(func, func_der, roots, parameter_samples)
+	print("Calc constant solution")
 	con = get_constant_solution(roots)
 
 	(best_sol, best_err, best_pars) = cgp_find_starting_guess(opt_data, func, func_der, parameter_generator, nr_of_parameters)
-	print("Cgp done")	
+	print("Cgp done")
 
 	n = len(roots)
 	print("Con:", con)
@@ -419,3 +494,47 @@ if __name__ == '__main__':
 	print(conv_factors)
 	print(total_time)
 	print(ops_running_times)
+	
+	print("Generating new parameter samples for testing")
+	# Get parameter samples
+	par_samples = generate_parameters(nr_of_parameters, parameter_generator)
+	limit = 1.0e-6
+
+	all_const_times = [0.0]*n
+	for i in range(n):
+		t0 = perf_counter()
+		parameters = par_samples[i]
+		newton_raphson(func, func_der, parameters, max_iter=10000, convg_lim=limit, x0=con)
+		t = perf_counter()-t0
+		all_const_times[i] = t
+
+	all_tay_times = [0.0]*n
+	for i in range(n):
+		t0 = perf_counter()
+		pars = par_samples[i]
+		c = tay.get_starting_guess(pars)
+		newton_raphson(func, func_der, parameters, max_iter=10000, convg_lim=limit, x0=c)
+		t = perf_counter()-t0
+		all_tay_times[i] = t
+
+	all_cgp_times = [0.0]*n
+	cgp_func = False
+	assert cgp_func
+	for i in range(n):
+		t0 = perf_counter()
+		pars = par_samples[i]
+		c = cgp_func(pars)
+		newton_raphson(func, func_der, parameters, max_iter=10000, convg_lim=limit, x0=c)
+		t = perf_counter()-t0
+		all_cgp_times[i] = t
+
+	all_const_times.sort()
+	all_tay_times.sort()
+	all_cgp_times.sort()
+
+	import matplotlib.pyplot as plt
+	plt.plot(all_const_times, 'r')
+	plt.plot(all_tay_times)
+	plt.plot(all_cgp_times)
+
+	plt.show()
